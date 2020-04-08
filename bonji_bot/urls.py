37192a9de13bf71c3b2a98ad49c14533edefd2e7
Dmytro <58703014+dberehovets@users.noreply.github.com>
2020-04-08 23:50:49 +0300

    pox

--- bonji_bot/urls.py
+++ bonji_bot/urls.py
@@ -6,6 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 app_name = "bonji_bot"
 
 urlpatterns = [
-    path("c04a4995-a7e2-4bf5-b8ab-d7599105d1d1/", csrf_exempt(UpdateBot.as_view()), name='update'),
+    path("c04a4995-a7e2-4bf5-b8ab-d7599105d1d1", csrf_exempt(UpdateBot.as_view()), name='update'),
     path("", csrf_exempt(Check.as_view()), name='check'),
 ]
